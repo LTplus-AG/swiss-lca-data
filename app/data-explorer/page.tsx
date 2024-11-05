e64e4f9e9c072107e1c57989068c0a71ae781ac9
Louis Trümpler <78563314+louistrue@users.noreply.github.com>
2024-11-05 13:05:32 +0100

    ui style

--- app/data-explorer/page.tsx
+++ app/data-explorer/page.tsx
@@ -3,8 +3,7 @@
 import DataExplorerChart from "../pages/app-data-explorer-page";
 
 export default function DataExplorer() {
-  // You might want to fetch the data here or pass it from a parent component
-  const data: Material[] = []; // Replace with actual data
+  const data: Material[] = [];
 
   return <DataExplorerChart data={data} />;
 }
